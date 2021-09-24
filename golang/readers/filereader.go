e4455f16d773e02cf2fa213986b6fc95f3576403
vvenkatesh1 <vvenkatesh@asapp.com>
2021-09-24 17:04:41 -0400

    Boilerplate file reading code

--- golang/readers/filereader.go
+++ golang/readers/filereader.go
@@ -1,6 +1,24 @@
+// Reads a file from a local path and pushes out one line at a time.
 package filereader
 
-// Reads a file from a local path and pushes out one line at a time.
-func Readline() string {
+import "os"
+
+type CodeFile struct {
+	lines string
+	//fileHandle *File
+	lineNumber int
+}
+
+func OpenFile(path string) error {
+	_, err := os.Open(path)
+
+	return err
+}
+/*
+func (CodeFile *f) ReadNextLine() string {
 	return ""
 }
+
+func (CodeFile *f) ReadPreviousLine() string {
+	return ""
+}*/
