347941d0013541a8323be2daae9e5b4f5e3e5d4f
vvenkatesh1 <vvenkatesh@asapp.com>
2021-09-24 16:09:38 -0400

    Some minor changes

--- golang/main/arghandlers.go
+++ golang/main/arghandlers.go
@@ -3,28 +3,16 @@ package main
 import "fmt"
 
 // Start an interactive interpreter
-func start_interpreter() {
+func Interpreter() {
 	fmt.Println(Usage())
 	fmt.Println("Starting interpreter...")
 	fmt.Println("> ")
 }
 
-// Validate subcommand and execute
-func handle_subcommand(subcommand string, args []string) {
-	switch subcommand {
-		case "run":
-			fmt.Println("Running lox file -", args[0])
-		case "build":
-			fmt.Println("Building...")
-		default:
-			fmt.Println("ERROR!!! Bad subcommand:", subcommand)
-	}
-}
-
 // Run the program
-func run(args []string) {
+func Run(args []string) {
 }
 
 // Build an application/module
-func build(args []string) {
+func Build(args []string) {
 }
--- golang/main/main.go
+++ golang/main/main.go
@@ -8,9 +8,21 @@ func main() {
 	args := os.Args[1:]
 
 	switch {
-		case len(args) == 0:		start_interpreter()
+		case len(args) == 0:		Interpreter()
 		case len(args) > 1:			handle_subcommand(args[0], args[1:])
 		default:								fmt.Println(Usage())
+	}
+}
+
+// Validate subcommand and execute
+func handle_subcommand(subcommand string, args []string) {
+	switch subcommand {
+		case "run":
+			fmt.Println("Running lox file -", args[0])
+		case "build":
+			fmt.Println("Building...")
+		default:
+			fmt.Println("ERROR!!! Bad subcommand:", subcommand)
 	}
 }
 
