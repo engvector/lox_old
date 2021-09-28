b768274854884ef1f9c5bad60a4c7d9f43e8d082
Vinay Venkatesh <vinay.venkatesh@protonmail.com>
2021-09-27 23:59:49 -0400

    Added error reporting

--- golang/main/arghandlers.go
+++ golang/main/arghandlers.go
@@ -1,6 +1,8 @@
 package main
 
-import "fmt"
+import (
+				"fmt"
+				"os")
 
 // Start an interactive interpreter
 func Interpreter() {
@@ -15,4 +17,14 @@ func Run(args []string) {
 
 // Build an application/module
 func Build(args []string) {
+}
+
+// Error reporting functions
+func ReportError(line int, message string) {
+	Report(line, "", message)
+}
+
+func Report(line int, where string, message string) {
+	fmt.Fprintln(os.Stderr,
+							"ERROR @",line,":",where,"-",message);
 }
