af706dbeae1defa20f6054bcff757b8be8ca0983
vvenkatesh1 <vvenkatesh@asapp.com>
2021-09-28 15:09:06 -0400

    Scanner detects symbols, keywords and quoted literals

--- golang/scanner/scanner.go
+++ golang/scanner/scanner.go
@@ -1,102 +1,89 @@
 package scanner
 
-import "errors"
+import "strings"
 
-type TokenType int32
-const (
-	// Single-character tokens.
-	LEFT_PAREN TokenType = iota
-	RIGHT_PAREN
-	LEFT_BRACE
-	RIGHT_BRACE
-  COMMA
-	DOT
-	MINUS
-	PLUS
-	SEMICOLON
-	SLASH
-	STAR
-	// One or two character tokens.
-  BANG
-	BANG_EQUAL
-  EQUAL
-	EQUAL_EQUAL
-  GREATER
-	GREATER_EQUAL
-  LESS
-	LESS_EQUAL
-  // Literals.
-  IDENTIFIER
-	STRING
-	NUMBER
-  // Keywords.
-  AND
-	CLASS
-	ELSE
-	FALSE
-	FUN
-	FOR
-	IF
-	NIL
-	OR
-  PRINT
-	RETURN
-	SUPER
-	THIS
-	TRUE
-	VAR
-	WHILE
-	// End of file
-  EOF
-)
+type TokenType string
 
 type Token struct {
-	Type TokenType
 	Lexeme string
 	Literal string
 }
 
 type Scanner struct {
-	tokens []Token
+	lexemes []string
 }
 
-func (s *Scanner) addToken(typ TokenType) {
-	s.tokens = append(s.tokens,
-										Token {
-											Type : typ,
-										})
-}
+func (s *Scanner) Scan(code string) ([]string, error) {
+	// Valid single character symbols.
+	singleCharSymbols := "{}(),.-+;/*"
+	// Single/Double character symbols are codified as two strings. First 
+	// string contains the first symbol and the second string contains the
+	// matching second symbol.
+	singleOrDoubleCharSymbols := []string{"!=><","===="}
+	keywords := []string{
+			"and", "class", "else", "false", "fun", "for",
+			"if", "nil", "or", "print", "return", "super",
+			"this", "true", "var", "while"}
 
-func (s *Scanner) Scan(code string) ([]Token, error) {
-	for index, character := range code {
-		switch character {
-			case '(':
-				s.addToken(LEFT_PAREN)
-      case ')':
-				s.addToken(RIGHT_PAREN)
-      case '{':
-				s.addToken(LEFT_BRACE)
-      case '}':
-				s.addToken(RIGHT_BRACE)
-      case ',':
-				s.addToken(COMMA)
-      case '.':
-				s.addToken(DOT)
-      case '-':
-				s.addToken(MINUS)
-      case '+':
-				s.addToken(PLUS)
-      case ';':
-				s.addToken(SEMICOLON)
-      case '*':
-				s.addToken(STAR)
-			case ' ':
-				continue
-			default:
-				return s.tokens,
-							 errors.New("unexpected character - " + code[index:index+1])
+	// First Pass: Break string into lexemes
+	for i := 0; i < len(code); i++ {
+		// First make sure symbols inside quotes are not detected.
+		if code[i] == '"' {
+			aString := ""
+			for i++; code[i] != '"'; i++ {
+				aString = aString + string(code[i])
+			}
+			s.lexemes = append(s.lexemes, aString)
+			continue // Move on
+		}
+		// Start with single character symbols
+		if strings.IndexByte(singleCharSymbols, code[i]) != -1 {
+			s.lexemes = append(s.lexemes, string(code[i]))
+			continue // Move on
+		}
+		// Check for single/double character symbols
+		if strings.IndexByte(singleOrDoubleCharSymbols[0], code[i]) != -1 {
+			if strings.IndexByte(singleOrDoubleCharSymbols[1], code[i+1]) != -1 {
+				// If both characters matched
+				s.lexemes = append(s.lexemes, string(code[i]) + string(code[i+1]))
+				i++
+			} else {
+				// If we only matched a single character
+				s.lexemes = append(s.lexemes, string(code[i]))
+			}
+			continue // Move on
+		}
+		// Look for keywords
+		kwd := findKeyword(code, i, keywords)
+		if (kwd != "") {
+			s.lexemes = append(s.lexemes, kwd)
+			i += len(kwd) - 1 // Fast-forward index to end of detected keyword
+			continue // Move on
 		}
 	}
-	return s.tokens, nil
+	return s.lexemes, nil
 }
 
+// Look for keywords starting at supplied index.
+// Returns the detected keyword.
+func findKeyword(input string, startIdx int, keywords []string) string {
+	for k := 0; k < len(keywords); k++ {
+		if matchKeyword(input[startIdx:], keywords[k]) {
+			return keywords[k]
+		}
+	}
+	return ""
+}
+
+// Use KMP algorithm to match keyword
+func matchKeyword(input string, keyword string) bool {
+	for i := 0; i < len(input); i++ {
+		for j := 0; j < len(keyword); j++ {
+			if input[i+j] != keyword[j] {
+				return false
+			}
+		}
+		return true
+	}
+	return false
+}
--- golang/scanner/scanner_test.go
+++ golang/scanner/scanner_test.go
@@ -3,12 +3,81 @@ package scanner
 import ("testing"
 				"log")
 
-func TestScanner(t *testing.T) {
+func TestSingleCharSymbolLexer(t *testing.T) {
+	symbols := "{}(),.-+;/*"
 	var scanner Scanner
+	tokens, err := scanner.Scan(symbols);
+	if err != nil {
+		t.Error(err)
+	}
+	for i := 0; i < len(tokens); i++ {
+		if string(symbols[i]) != tokens[i] {
+			t.Error("Expected:", string(symbols[i]), "Found:", tokens[i])
+		}
+	}
+	log.Print(tokens)
+}
+
+func TestSingleOrDoubleCharSymbolLexer(t *testing.T) {
+	symbols := "!!====>>=<<="
+	expectedResult := []string{"!","!=","==","=",">",">=","<","<="}
+	var scanner Scanner
+	tokens, err := scanner.Scan(symbols);
+	if err != nil {
+		t.Error(err)
+	}
+	for i := 0; i < len(tokens); i++ {
+		if expectedResult[i] != tokens[i] {
+			t.Error("Expected:", expectedResult[i], "Found:", tokens[i])
+		}
+	}
+	log.Print(tokens)
+}
+
+func TestAllSymbols(t *testing.T) {
+	text := "{ !(!=) == (>) = * }"
+	expectedResult := []string {"{","!","(","!=",")","==",
+															"(",">",")","=","*","}"}
+	var scanner Scanner
+	tokens, err := scanner.Scan(text);
+	if err != nil {
+		t.Error(err)
+	}
+	for i := 0; i < len(tokens); i++ {
+		if expectedResult[i] != tokens[i] {
+			t.Error("Expected:", expectedResult[i], "Found:", tokens[i])
+		}
+	}
+	log.Print(tokens)
+}
+
+func TestKeywordDetection(t *testing.T) {
+	text := "pri print \"ABC\";" // This is an invalid lox syntax.
+	expectedResult := []string {"print", "ABC", ";"}
+	var scanner Scanner
+	tokens, err := scanner.Scan(text)
+	if err != nil {
+		t.Error(err)
+	}
+	for i := 0; i < len(tokens); i++ {
+		if expectedResult[i] != tokens[i] {
+			t.Error("Expected:", expectedResult[i], "Found:", tokens[i])
+		}
+	}
+	log.Print(tokens)
+}
+
+func TestQuotedText(t *testing.T) {
+	var scanner Scanner
+	expectedTokens := []string{"print", "Hello world!", ";"}
 	tokens, err := scanner.Scan("print \"Hello world!\";")
 	if err != nil {
 		t.Error(err)
 	}
-
+	for i := 0; i < len(tokens); i++ {
+		if expectedTokens[i] != tokens[i] {
+			t.Error("Found:",tokens[i]," Expected:", expectedTokens[i])
+		}
+	}
 	log.Print(tokens)
 }
